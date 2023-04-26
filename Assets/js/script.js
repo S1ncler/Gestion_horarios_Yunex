9ca8b8695229ad456481212e12c425b178aa8fcf
S1ncler <daniel_felipe_c11@hotmail.com>
2023-04-25 19:41:15 -0500

    contenido de la pagina mediante jquery y llenado de las tarjetas automatico

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -22,13 +22,30 @@ fetch("footer.html")
         footer.innerHTML = html;
     })
 
-window.addEventListener(`load`, function() {
-    document.querySelector("#form-login").addEventListener(`submit`, function(event) {
+window.addEventListener(`load`, function () {
+    document.querySelector("#form-login").addEventListener(`submit`, function (event) {
         event.preventDefault();
         fetch("main.html")
             .then(response => response.text())
             .then(html => {
                 main.innerHTML = html;
+                const cardContainer = document.querySelector('[card-container]');
+                fetch("card.html")
+                    .then(reponse => reponse.text())
+                    .then(html => {
+                        let cards = "";
+                        for (let i = 0; i <= 4; i++) cards += html;
+                        cardContainer.innerHTML = cards
+                        const titulostarjetas = document.querySelectorAll('[titulo-tarjeta]')
+                        const meses = ["enero-febrero", "febrero-marzo", "marzo-abril", "abril-mayo", "mayo-junio"]
+                        for (let i in meses) titulostarjetas[i].innerHTML = meses[i]
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+            })
+            .catch(error => {
+                console.log(error)
             })
     })
 })
